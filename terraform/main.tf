5438626b8dc9ec7950d2217a3930b14a200c180b
Tom Morris <thomasmorris823@gmail.com>
2024-02-04 16:42:37 +0000

    Remove module and replace with git source ref

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,29 +6,20 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "eu-west-2"
-}
-
-provider "aws" {
-  region = "us-east-1"
-  alias  = "use1"
-}
-
 locals {
-    domain_name = "hapihour.io"
-    root_domain_name = "hapihour.io"
-    allowed_locations = ["GB"]
-    common_tags = {
-        Project = "hapihour.io"
-    }
+  domain_name       = "hapihour.io"
+  root_domain_name  = "hapihour.io"
+  allowed_locations = ["GB"]
+  common_tags = {
+    Project = "hapihour.io"
+  }
 }
 
 module "static-website" {
-  source = "./modules/static-website"
+  source = "git@github.com:TomBenjaminMorris/tf-modules.git//static-website?ref=static-website-v0.0.1"
 
   domain_name       = local.domain_name
   root_domain_zone  = local.root_domain_name
   allowed_locations = local.allowed_locations
-  common_tags = local.common_tags
+  common_tags       = local.common_tags
 }
